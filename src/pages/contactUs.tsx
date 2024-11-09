bf7cc28cab6661c9b4061ea581ccf70ae4dc8225
Neelesh Ravichandran <neelesh.ravichandran1999@gmail.com>
2024-11-09 18:12:35 +0000

    WIP updates

--- src/pages/contactUs.tsx
+++ src/pages/contactUs.tsx
@@ -28,7 +28,7 @@ function getButton(props: {
     }
 
     return <div
-        className="text-center text-white bg-agrim py-2 px-1 rounded-lg cursor-pointer hover:bg-agrimDark transition duration-300 w-32"
+        className="text-center text-white bg-agrim py-2 px-1 rounded-md cursor-pointer hover:bg-agrimDark transition duration-300 w-32"
     onClick={() => handleSend()}>
         {props.name}
         <ConfirmationPopUp isOpen={isOpen} close={() => setOpen(false)} />
@@ -39,7 +39,8 @@ interface EmailProps {
     name: string,
     company: string
     email: string,
-    message: string
+    message: string,
+    phone: string
 }
 
 function sendEmail(props: EmailProps) {
@@ -48,7 +49,8 @@ function sendEmail(props: EmailProps) {
         from_name: props.name,
         message: props.message,
         company_name: props.company,
-        reply_to: props.email
+        reply_to: props.email,
+        phone_no: props.phone,
     }, '0EtUNp83sr_-uKAK8');
 }
 
@@ -57,24 +59,55 @@ function ContactUsPage() {
     const [nameState, setNameState] = useState("");
     const [messageState, setMessageState] = useState("");
     const [emailState, setEmailState] = useState("");
+    const [phoneState, setPhoneState] = useState("");
     const [companyState, setCompanyState] = useState("");
 
     return <div>
-        <div className="w-full text-slate-900 text-center font-bold text-4xl p-4">
+        <div className="w-full text-slate-900 text-center text-5xl p-4 pt-6">
             Contact Us
         </div>
         <div className="flex flex-col space-y-4 w-2/3 mx-auto pb-4">
             <EntryBox label="Name" update={setNameState} />
             <EntryBox label="Company name" update={setCompanyState} />
             <EntryBox label="Your email" update={setEmailState} />
+            <EntryBox label="Your phone number" update={setPhoneState} />
             <EntryBoxLarge label="Your message" update={setMessageState} />
         </div>
-        <div className="p-16 mx-auto w-fit">
+        <div className="p-8 mx-auto w-fit">
             {getButton({name: "Submit", emailProps: {
                     name: nameState,
                     company: companyState,
                     email: emailState,
-                    message: messageState}})}
+                    message: messageState,
+                    phone: phoneState,}})}
+        </div>
+        <div className="w-full text-slate-900 text-center text-3xl p-4">
+            Our offices
+        </div>
+        <div className="flex flex-row justify-between py-4 pb-8">
+            <div className="flex flex-col w-1/4">
+                <div className="text-center text-xl">Indonesia</div>
+                <div className="bg-agrim h-1 w-full mb-2"></div>
+                <div className="text-center">PT. JASUM JAYA</div>
+                <div className="text-center">Medan, Indonesia</div>
+                <div className="font-bold text-center">+62 811 6000 625</div>
+            </div>
+            <div className="flex flex-col w-1/4">
+                <div className="text-center text-xl">Singapore</div>
+                <div className="bg-agrim h-1 w-full mb-2"></div>
+                <div className="text-center">AGRIM PTE LTD</div>
+                <div className="text-center">60 Paya Lebar Road,</div>
+                <div className="text-center">#09-43 Paya Lebar Square,</div>
+                <div className="text-center">Singapore - 409051</div>
+                <div className="font-bold text-center">+65 9169 4785</div>
+            </div>
+            <div className="flex flex-col w-1/4">
+                <div className="text-center text-xl">Vietnam</div>
+                <div className="bg-agrim h-1 w-full mb-2"></div>
+                <div className="text-center">TAN HUA AGRO INDUSTRIES</div>
+                <div className="text-center">Hanoi, Vietnam</div>
+                <div className="font-bold text-center">+84 814 570 625</div>
+            </div>
         </div>
     </div>
 }
