3b1681acbf2bf0e56475caf361aa1605c8e7b316
Neelesh Ravichandran <neelesh.ravichandran1999@gmail.com>
2025-03-08 17:12:39 +0000

    about us page done

--- src/pages/aboutUs.tsx
+++ src/pages/aboutUs.tsx
@@ -1,115 +1,197 @@
 import companyPic from "../assets/companyPic.png"
-import firstAbout from "../assets/original/firstAbout.jpeg"
-import secondAbout from "../assets/original/secondAbout.jpeg"
-import qualityLogo from "../assets/original/TQSCI.jpeg"
-import gmp from "../assets/original/gmp.jpeg"
-import haccp from "../assets/original/haccp.jpeg"
-import factoryImg from "../assets/original/factory-icon.png"
-import hygene from "../assets/original/gloves-icon.png"
-import skilled from "../assets/original/skills-icon.png"
-import lab from "../assets/original/lab-icon.png"
+import tick from "../assets/tick.png"
+import stockPrice from "../assets/stockPrice.png"
+import shield from "../assets/shield.png"
+import leaves from "../assets/leaves.png"
+import workersWorking from "../assets/original/workersWorking.jpeg"
+import handouts from "../assets/original/handout.jpeg"
+import multitasker from "../assets/original/icon-multitasker.png"
+import gloves from "../assets/original/icon-gloves.png"
+import lab from "../assets/original/icon-lab.png"
+import factory from "../assets/original/icon-factory.png"
 import {useEffect} from "react";
-
-function choiceCard(number: string, title: string, text: string) {
-    return <div className="flex flex-col h-fit w-1/4">
-        <div className="flex flex-row">
-            <div className="text-5xl pr-2 align-text-bottom">{number}</div>
-            <div className="flex flex-col justify-end pb-2 w-full">
-                <div className="text-lg text-left">{title}</div>
-                <div className="w-full h-1 bg-agrim"></div>
-            </div>
-        </div>
-        <div className="text-left">
-            {text}
-        </div>
-    </div>
-}
-
+import gmp from "../assets/original/gmp.jpeg";
+import haccp from "../assets/original/haccp.jpeg";
+import qualityLogo from "../assets/original/TQSCI.jpeg";
 function AboutUsPage() {
 
     useEffect(() => {
         window.scrollTo({ top: 0})
     }, [])
 
-    const card1Text = "By optimising our processes, we ensure full visibility across the supply chain, from procurement to delivery. Each product is customised according to the customer's specific requirements."
-    const card2Text = "As an ISO, GMP, and HACCP-certified company, we adhere to the highest international standards in every aspect of our operations. These certifications reflect our dedication to delivering safe, premium-quality products."
-    const card3Text = "Being in close proximity to raw materials gives us the advantage of having a shorter lead time and keeping low inventory. This enables us to reduce waste and prevent expiry of products, ensuring the freshest, highest-quality products."
-    return <div>
-        <div className="w-full text-slate-900 text-center text-5xl p-4">
+    return <div className="flex flex-col">
+        <div className="w-full text-slate-900 text-center text-3xl p-4 my-3">
             About Us
         </div>
-        <div className="flex flex-col space-y-4 mx-auto w-3/4 pb-4 text-center">
-            <div>
-                Founded in 2004, Agrim began as a small trading company focused on agricultural products. With a
-                commitment to quality and sustainable practices, we’ve grown steadily, earning a reputation for
-                delivering reliable service and premium products. From sourcing to shipment, we ensure strict quality
-                control at every stage, while working closely with local farmers and following fair trade practices.
-                <br/><br/>
-                Today, Agrim specialises in the procurement, processing, trading, and supply of spices and coconut
-                ingredients from Indonesia and Vietnam. Our expertise has allowed us to build a global customer base.
-                Our endorsed facilities in Medan, Surabaya, Pontianak, and Hanoi meet the highest standards of quality
-                and safety, including FSSC-22000, ISO-9001, HACCP, and GMP certifications.
-            </div>
-            <div className="pt-6 pb-12">
-                <img src={companyPic} className="w-3/5 mx-auto"/>
-            </div>
-            <div className="w-full text-slate-900 text-center text-3xl p-4">
-                Why choose us?
-            </div>
-            <div className="flex flex-row justify-between pb-12">
-                {choiceCard("1", "Productivity & Efficiency", card1Text)}
-                {choiceCard("2", "Quality", card2Text)}
-                {choiceCard("3", "Origination", card3Text)}
-            </div>
-            <div className="flex flex-row w-full pt-6 pb-6">
-                <img src={firstAbout} className="max-h-64 w-5/12 mx-auto object-cover"/>
-                <img src={secondAbout} className="max-h-64 w-1/2 mx-auto object-cover"/>
-            </div>
-            <div className="w-full text-slate-900 text-center text-3xl p-4">
-                Our team
-            </div>
-            <div>
-                With the help of our experienced and dedicated workforce, we have positioned ourselves as a strong
-                contender in the market.
-                We take pride in our sincere and committed team of 120, who work closely together to achieve our goals
-                efficiently.
-                We provide comprehensive on-the-job training and regularly hold workshops to continually enhance the
-                skills and expertise of our staff.
-            </div>
-            <div className="w-full text-slate-900 text-center text-3xl p-4 pt-10">
-                Our Facility
-            </div>
-            <div className="flex flex-row justify-between">
-                <div className="flex flex-col">
-                    <img src={factoryImg} className="h-32 object-cover"/>
-                    <div>20,000 sqm factory</div>
+        <div className="flex flex-col space-y-4 mx-auto w-full pb-4">
+            <div className="grid grid-rows-1 grid-cols-8 flex-row w-full gap-8">
+                <div className="col-span-5">
+                    <img src={companyPic} className="w-full rounded-2xl"/>
                 </div>
-                <div className="flex flex-col">
-                    <img src={hygene} className="h-32 object-cover"/>
-                    <div>Hygiene work space</div>
-                </div>
-                <div className="flex flex-col">
-                    <img src={skilled} className="h-32 object-cover"/>
-                    <div>Skilled & trained workers</div>
-                </div>
-                <div className="flex flex-col">
-                    <img src={lab} className="h-32 object-cover"/>
-                    <div>Equipped in-house laboratory</div>
-                </div>
-            </div>
-            <div className="w-full h-fit flex flex-col pt-10">
-                <div className="w-full p-4 mx-auto flex flex-col mb-4">
-                    <div className="text-center text-3xl">Certifications</div>
-                    <div className="text-center text-xl">Our factory is accredited with ISO-9001, HACCP, GMP
-                        Certifications
+                <div className="col-span-3">
+                    <div className="text-2xl text-slate-900 mt-4">
+                        Indonesian Origin
+                    </div>
+                    <div className="h-1 bg-agrim w-48 my-1 rounded-full">
+
+                    </div>
+                    <div className="text-sm">
+                        Founded in 2004, Agrim has grown from a small agricultural trading company into a trusted
+                        provider of premium products and reliable service.
+                        <br/>
+                        <br/>
+                        Committed to quality and sustainability, we ensure strict quality control from sourcing to
+                        shipment, partner with local farmers, and uphold fair trade practices.
                     </div>
                 </div>
-                <div className="w-11/12 h-52 grid grid-rows-1 grid-cols-3 mx-auto mb-12">
-                    <div className="mx-auto"><img className="h-52 object-cover" src={gmp}/></div>
-                    <div className="mx-auto"><img className="h-52 object-cover" src={haccp}/></div>
-                    <div className="mx-auto"><img className="h-52 object-cover" src={qualityLogo}/></div>
+            </div>
+        </div>
+        <div
+            className="w-full h-64 bg-agrimdarkgreen rounded-2xl mt-16 grid grid-rows-1 grid-cols-2 text-white px-12">
+            <div className="my-6 px-5">
+                <div className="text-2xl mt-4">
+                    Agrim today
+                </div>
+                <div className="h-1 bg-agrim w-32 my-1 rounded-full">
+
+                </div>
+                <div className="mt-3 text-sm">
+                    We are dedicated to providing pure, high-quality products while fostering trusted,
+                    transparent relationships with customers and suppliers. By supporting supplier productivity,
+                    buyer growth, and employee satisfaction, we aim to drive mutual success and sustainable
+                    business growth.
                 </div>
             </div>
+            <div className="mt-6 grid grid-cols-1 grid-rows-3 pr-24">
+                <div className="flex flex-row gap-x-4 h-12">
+                    <img src={tick} className="w-6 h-6"/>
+                    <div className="text-sm">Specialise in procurement, processing and supplying of spices &
+                        coconut ingredients.
+                    </div>
+                </div>
+                <div className="flex flex-row gap-x-4 h-12">
+                    <img src={tick} className="w-6 h-6"/>
+                    <div className="text-sm">A global customer base with exports to 80+ countries.
+                    </div>
+                </div>
+                <div className="flex flex-row gap-x-4 h-12">
+                    <img src={tick} className="w-6 h-6"/>
+                    <div className="text-sm">High standard facilities with FSSC-22000, ISO-9001, HACCP and GMP
+                        certifications.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="bg-darkness my-10 h-80 text-white flex flex-col space-y-4 w-full px-12 py-8">
+            <div className="flex flex-col">
+                <div className="text-2xl mt-4">
+                    Why choose us?
+                </div>
+                <div className="h-1 bg-agrim w-40 my-1 rounded-full">
+
+                </div>
+            </div>
+            <div className="grid grid-rows-1 grid-cols-3">
+                <div className="flex flex-row h-56 w-80">
+                    <div className="w-1/6 right-0">
+                        <img src={stockPrice} className="w-12 h-8"/>
+                    </div>
+                    <div className="w-5/6 flex flex-col">
+                        <div className="text-2xl">Productivity & Efficiency</div>
+                        <div className="text-sm">We optimise processes to ensure full supply chain visibility and customise each product to
+                            meet customer requirements.
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-row h-56 w-80">
+                    <div className="w-1/6 right-0">
+                        <img src={shield} className="w-12 h-8"/>
+                    </div>
+                    <div className="w-5/6 flex flex-col">
+                        <div className="text-2xl">Quality</div>
+                        <div className="text-sm">With ISO, GMP, and HACCP certifications, we uphold the highest international standards,
+                            ensuring safe, premium-quality products.
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-row h-56 w-80">
+                    <div className="w-1/6 right-0">
+                        <img src={leaves} className="w-12 h-8"/>
+                    </div>
+                    <div className="w-5/6 flex flex-col">
+                        <div className="text-2xl">Origination</div>
+                        <div className="text-sm">Our proximity to raw materials ensures shorter lead times and low inventory, reducing waste
+                            and preventing product expiry for fresher, high-quality offerings.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="grid grid-rows-1 grid-cols-8 flex-row w-full gap-8 my-20">
+            <div className="col-span-5">
+                <img src={workersWorking} className="w-full rounded-2xl"/>
+            </div>
+            <div className="col-span-3">
+                <div className="text-2xl text-slate-900 mt-4">
+                    Our team
+                </div>
+                <div className="h-1 bg-agrim w-48 my-1 rounded-full">
+
+                </div>
+                <div className="text-sm">
+                    Our dedicated team of 120 professionals drives our market strength through collaboration and
+                    efficiency. We invest in comprehensive training and regular workshops to continually enhance their
+                    skills and expertise.
+                </div>
+            </div>
+        </div>
+        <div className="grid grid-rows-1 grid-cols-8 flex-row w-full gap-8 my-20">
+            <div className="col-span-4">
+                <div className="flex flex-col">
+                    <div className="text-2xl mt-4">
+                        Our facility
+                    </div>
+                    <div className="h-1 bg-agrim w-32 my-1 rounded-full">
+
+                    </div>
+                </div>
+                <div className="grid grid-cols-2 grid-rows-2 p-6">
+                    <div className="flex flex-col">
+                        <img src={factory} className="h-28 w-40 mx-auto"/>
+                        <div className="text-center">20,000 sqm factory</div>
+                    </div>
+                    <div className="flex flex-col">
+                        <img src={gloves} className="h-28 w-40  mx-auto"/>
+                        <div className="text-center">Hygiene work space</div>
+                    </div>
+                    <div className="flex flex-col">
+                        <img src={lab} className="h-28 w-40  mx-auto"/>
+                        <div className="text-center">Equipped in-house laboratory</div>
+                    </div>
+                    <div className="flex flex-col">
+                        <img src={multitasker} className="h-28 w-40  mx-auto"/>
+                        <div className="text-center">Skilled & trained workers</div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-span-4">
+                <img src={handouts} className="w-full rounded-2xl"/>
+            </div>
+        </div>
+        <div className="w-full p-4 mx-auto flex flex-col mt-4 space-y-4">
+            <div className="flex flex-col mx-auto">
+                <div className="text-center text-3xl">Certifications</div>
+                <div className="h-1 bg-agrim w-44 my-1 rounded-full">
+
+                </div>
+            </div>
+            <div className="text-center text-xl">Our factory is accredited with ISO-9001, HACCP, GMP Certifications
+            </div>
+        </div>
+        <div className="w-11/12 h-52 grid grid-rows-1 grid-cols-3 mx-auto mb-12 mt-4">
+            <div className="mx-auto"><img className="h-52 object-cover" src={gmp}/></div>
+            <div className="mx-auto"><img className="h-52 object-cover" src={haccp}/></div>
+            <div className="mx-auto"><img className="h-52 object-cover" src={qualityLogo}/></div>
         </div>
     </div>
 }
